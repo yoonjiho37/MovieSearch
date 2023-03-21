579ef182ecffcf00dac56d0456f960e79b718a8d
yoonjiho37 <yoon.jiho37@gmail.com>
2023-03-22 01:01:42 +0900

    [ADD] Xcode File / Git Ignore

---
+++ MovieSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by 윤지호 on 2023/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
