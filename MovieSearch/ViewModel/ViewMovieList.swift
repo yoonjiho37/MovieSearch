031f76bebba5bd7870458b4f52ec26c96e79c0d3
yoonjiho37 <yoon.jiho37@gmail.com>
2023-04-13 23:14:46 +0900

    [Refactor] ViewMovieList init

--- MovieSearch/ViewModel/ViewMovieList.swift
+++ MovieSearch/ViewModel/ViewMovieList.swift
@@ -11,39 +11,39 @@ import RxSwift
 class ViewMovieList {
     let movieId: String  //등록ID    K
     let title: String  //영화명    서편제
-//    let directorNm: String  //감독명    김지훈
-//    let actors: Actors  //배우명    하지원
+    let directorNm: String  //감독명    김지훈
+    let actors: Actors  //배우명    하지원
     let company: String  //제작사    ㈜JK필름,CJ E&M Pictures
-//    let plot: String  //줄거리    제주도 남단, 7광구의.....
+    let plot: String  //줄거리    제주도 남단, 7광구의.....
     let runtime: String  //대표상영시간    112
     let rating: String  //대표관람등급    15세관람가
     let genre: String  //kmdbUrl    액션,SF
-    let posterURL: String
+    let posterURL: [String]
     
     let rank: Int
     
     init(info: MovieInfo) {
         self.movieId = info.movieId
         self.title = info.title.removeBlank()
-//        self.directorNm = info.directors.director[0].directorNm
-//        self.actors = info.actors
+        self.directorNm = info.directors.director[0].directorNm
+        self.actors = info.actors
         self.company = info.company
-//        self.plot = info.plots.plot[0].plotText
+        self.plot = info.plots.plot[0].plotText
         self.runtime = info.runtime
         self.rating = info.rating
         self.genre = info.genre
-        self.posterURL = info.posters
+        self.posterURL = info.posters.components(separatedBy: "|")
         self.rank = 0
     }
     
     
-    init(movieId: String, title: String, company: String, runtime: String, rating: String, genre: String, posterURL: String, rank: Int) {
+    init(movieId: String, title: String, directorNm: String, actors: Actors, company: String, plot: String, runtime: String, rating: String, genre: String, posterURL: [String], rank: Int) {
         self.movieId = movieId
         self.title = title
-//        self.directorNm = directorNm
-//        self.actors = actors
+        self.directorNm = directorNm
+        self.actors = actors
         self.company = company
-//        self.plot = plot
+        self.plot = plot
         self.runtime = runtime
         self.rating = rating
         self.genre = genre
